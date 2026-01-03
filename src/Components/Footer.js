cd2baad34c363d98d7e63d020ecb8fb5818e5f15
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-01-03 15:38:16 +0600

    Footer Component

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <h6 className='bg-dark text-white text-center p-3' style={{position: "fixed", bottom: 0, width: "100%"}}>
+            © 2024 Word Counter. All rights reserved.</h6>
+      </footer>
+    </div>
+  )
+}
