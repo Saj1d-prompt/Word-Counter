43bd362a6ea0413d319ffca1ad7e649867e09a6e
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-01-03 15:11:21 +0600

    Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <h1 className='bg-primary text-white text-start p-3'>Word Counter</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
