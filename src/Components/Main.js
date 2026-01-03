bcc422b41a63051c535262fead64208036222d51
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-01-03 21:38:07 +0600

    Full Project

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 
 export default function Main() {
-  const [ text, setText ] = React.useState("");
+  const [ text, setText ] = useState("");
   const handleChange=(event)=>{
     setText(event.target.value);
   }
@@ -28,6 +28,24 @@ export default function Main() {
             <button type="button" className="btn btn-outline-info mx-2" onClick={makeLowerCase}>Make Lower Case</button>
             <button type="button" className="btn btn-outline-danger mx-2" onClick={clearText}>Clear Text</button>
         </div>
+        <div className="row justify-content-center align-items-center">
+          <div className="col-sm-4 mb-3 mb-sm-0">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Counting Word</h5>
+                <p className="card-text">You have Written <b>{text.split(" ").length}</b> Words</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Counting Character</h5>
+                <p className="card-text">You have Written <b>{text.length}</b> Characters</p>
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
